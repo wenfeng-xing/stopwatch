468b44eb8f0936b170edc8d1acfb6faeed44cde3
wenfeng xing <wenfeng.xing@weareadaptive.com>
2022-06-14 15:12:44 -0400

    remove the unused file

--- service/lapNodeCreation.view.js
+++
@@ -1,9 +0,0 @@
-import HtmlNodeCreation from "./htmlNodeCreation.view";
-
-export default class LapNodeCreation extends HtmlNodeCreation{
-    constructor(lapNumText, lapTimeText) {
-        const lapNumNode = new HtmlNodeCreation('p').addClassName('count-lap').addInnerHTML(lapNumText).build();
-        const lapTimeNode = new HtmlNodeCreation('p').addClassName('time').addInnerHTML(lapTimeText).build();
-        super(new HtmlNodeCreation('li').addChildNode(lapNumNode).addChildNode(lapTimeNode)) ;
-    }
-}
