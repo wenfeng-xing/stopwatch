2ce29e43b73c3b3c08a0ab3f2e3f03706f6129e6
wenfeng xing <wenfeng.xing@weareadaptive.com>
2022-06-14 15:10:05 -0400

    add the timer model

--- model/timer.model.js
+++ model/timer.model.js
@@ -1,7 +1,12 @@
-import { timeDifferenceGenerator } from "../utils/timeFormatter.js";
+import timeDifferenceGenerator from "../utils/timeDifferenceGenerator.js";
+import { formatTimeToText } from "../utils/timeFormatter.js";
 
 export default class Timer {
     constructor() {
+        this.timer = timeDifferenceGenerator();
+    }
+
+    resetTimer() {
         this.timer = timeDifferenceGenerator();
     }
 
